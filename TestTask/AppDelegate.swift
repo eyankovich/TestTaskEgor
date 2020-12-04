e45978e491655cb95419360e05e286ea37e49f88
Egor <superzzzzet@gmail.com>
2020-12-05 01:58:53 +0300

    - Added Load Data Button

--- TestTask/AppDelegate.swift
+++ TestTask/AppDelegate.swift
@@ -14,14 +14,21 @@ var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = ViewController()
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
-        return true
-
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        if let window = window {
+            let navigationController = UINavigationController()
+            navigationController.viewControllers = [ViewController()]
+            
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
         }
+        
+        return true
     }
+
+   
+}
 
    
 
--- TestTask/ViewController.swift
+++ TestTask/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setTabButtons()
         view.backgroundColor = .yellow
     }
-
-
+    
+    private func setTabButtons() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Load Data", style: .plain, target: self, action: #selector(loadData))
+    }
+    
+    @objc private func loadData(){
+        print("worked")
+    }
+    
 }
 
