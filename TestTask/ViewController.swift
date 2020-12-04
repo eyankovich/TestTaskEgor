7c8776be299aac25295c3bfa6b676183cd9ad6bf
Egor <superzzzzet@gmail.com>
2020-12-05 01:12:37 +0300

    First commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Егор Янкович on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
